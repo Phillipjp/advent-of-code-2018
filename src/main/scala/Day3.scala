ada26c2b59718ad47b3cabb70db90d943c73863c
Phillip Perks <p.perks@elsevier.com>
2018-12-04 11:27:23 +0000

    day 3 part 2

--- src/main/scala/Day3.scala
+++ src/main/scala/Day3.scala
@@ -18,10 +18,27 @@ object Day3 {
 
   def main(args: Array[String]): Unit = {
     val plans = Utils.fileToSeqElfPlans("day3input.txt")
+    //Part 1
     val squaresPerClaim = plans.flatMap(x => planClaim(x._2, x._3, x._4,x._5))
       .groupBy(identity).mapValues(_.size)
       .filter(_._2 > 1)
     val repeatedSquares = squaresPerClaim.size
     println(repeatedSquares)
+
+    //Part 2
+    val allPatterns = plans.map(x => (x._1, planClaim(x._2, x._3, x._4,x._5).toSet))
+
+    val uniqueSquares = plans.flatMap(x => planClaim(x._2, x._3, x._4,x._5))
+      .groupBy(identity).mapValues(_.size)
+      .filter(_._2  == 1).keys.toSet
+
+    val uniquePattern = allPatterns.map(x => (x._1, x._2--uniqueSquares)).filter(_._2.isEmpty).map(_._1).head
+    println(uniquePattern)
+
+
+
+
+
+
   }
 }
